eb1d7a8b63623d18b2979ce2ade4c19efd692dac
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-02-29 21:42:50 +0100

    ✨ Basic WezTerm Configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme_dirs = { "~/.config/wezterm/colors" },
+
+	-- Earthsong, Grey-green, GruvboxDark, niji, SeaShells, Spacedust, terafox, Twilight
+	color_scheme = "Everforest Dark (Medium)",
+
+	font = wezterm.font("JetBrains Mono"),
+	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+	font_size = 12.0,
+
+	enable_tab_bar = false,
+
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 16,
+	window_decorations = "RESIZE",
+}
