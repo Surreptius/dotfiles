e4efc5cb0df64f5a57be773c529d1b010aaf1215
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-03-03 23:52:28 +0100

    🔧 Initial settings for new setup

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neanias/everforest-nvim",
+    config = function()
+      require("everforest").setup({
+        transparent_background_level = 2,
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
