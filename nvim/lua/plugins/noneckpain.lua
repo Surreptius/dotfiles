2bbd1339fb4e5152c45e13034ad8ba9b4351d15a
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-03-03 23:57:09 +0100

    ➕ clamp for reading

---
+++ nvim/lua/plugins/noneckpain.lua
@@ -0,0 +1,9 @@
+return {
+  -- add symbols-outline
+  {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = "NoNeckPain",
+    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "No Neckpain" } },
+    opts = {},
+  },
+}
