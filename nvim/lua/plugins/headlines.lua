f7e4090efa2d037f3665ffd2f65bac6cc41d176c
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-03-04 17:04:39 +0100

    🔧 changed md header to vanilla look

---
+++ nvim/lua/plugins/headlines.lua
@@ -0,0 +1,12 @@
+return {
+  "lukas-reineke/headlines.nvim",
+  opts = {
+    markdown = {
+      headline_highlights = false,
+      bullets = { "", "󰫢", "󰫤", "󰣏" },
+      fat_headlines = true,
+      fat_headline_upper_string = "▃",
+      fat_headline_lower_string = "🬂",
+    },
+  },
+}
