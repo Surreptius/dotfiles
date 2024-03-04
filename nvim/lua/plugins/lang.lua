358d0fcb60668d1be449dffea6ce5aee69c44e51
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-03-04 17:03:42 +0100

    🔧 config for easy language expansion outside of lazy-extras

---
+++ nvim/lua/plugins/lang.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "astro", "vue", "markdown" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "vue-language-server", "astro-language-server", "marksman" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        astro = {},
+        volar = {},
+        marksman = {
+          filetypes = { "markdown", "markdown.mdx", "pandoc" },
+        },
+      },
+    },
+  },
+}
