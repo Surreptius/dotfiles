42e349c07cafe80174d6b49271a2c13b4a53ef33
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-03-04 17:02:21 +0100

    🔧 changed neotree behaviour

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+    },
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+    },
+  },
+}
