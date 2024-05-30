ee0a754210fc092e80315f6dbbf5f7b3be9202bc
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-05-30 22:37:26 +0200

    🔧 Changes due to lazyvim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("luasnip").config.set_config({ -- Setting LuaSnip config
-
-  -- Use Tab (or some other key if you prefer) to trigger visual selection
-  store_selection_keys = "<Tab>",
-})
