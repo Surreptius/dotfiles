0a8e302c4c6a644b46f93dc82498e0864559f424
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-06-26 15:38:38 +0200

    Added bandaid for marking in markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- MarkdownHighlight
+-- Create an autocommand group for markdown specific settings
+vim.api.nvim_create_augroup("MarkdownHighlight", { clear = true })
+
+-- Define the autocommand
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  group = "MarkdownHighlight",
+  callback = function()
+    -- Define the syntax group for lines starting with §
+    vim.cmd([[syntax match SectionLine /^§.*/]])
+
+    -- Link the SectionLine group to an existing highlight group
+    vim.cmd([[highlight SectionLine guifg=#7FBBB3]])
+  end,
+})
