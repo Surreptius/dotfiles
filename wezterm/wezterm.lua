81566db06e9843fae3806d5b6024eb8497b6de57
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-07-28 01:57:28 +0200

    Revamped config completely

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,47 @@
+-- Initialize Configuration
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+local opacity = 0.75
+-- local transparent_bg = "rgba(22, 24, 26, " .. opacity .. ")"
 
-return {
-	color_scheme_dirs = { "~/.config/wezterm/colors" },
+-- Font
+config.font = wezterm.font_with_fallback({
+	{
+		family = "Cascadia Code NF",
+		weight = "Regular",
+	},
+	"Segoe UI Emoji",
+})
+config.font_size = 12
 
-	-- Earthsong, Grey-green, GruvboxDark, niji, SeaShells, Spacedust, terafox, Twilight
-	color_scheme = "Everforest Dark (Medium)",
+-- Window
+config.initial_rows = 45
+config.initial_cols = 180
+config.window_decorations = "RESIZE"
+config.window_background_opacity = opacity
+config.window_close_confirmation = "NeverPrompt"
+config.win32_system_backdrop = "Acrylic"
+config.max_fps = 144
+config.animation_fps = 60
+config.cursor_blink_rate = 250
 
-	font = wezterm.font("ComicShannsMono Nerd Font"),
-	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
-	font_size = 14.0,
+-- Colors
+config.color_scheme = "One Dark (Gogh)"
+config.force_reverse_video_cursor = true
 
-	enable_tab_bar = false,
+-- Shell
+config.default_domain = "WSL:Arch"
 
-	window_background_opacity = 0.8,
-	macos_window_background_blur = 16,
-	window_decorations = "RESIZE",
+-- Tabs
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.show_tab_index_in_tab_bar = false
+config.use_fancy_tab_bar = false
+
+-- Keybindings
+config.keys = {
+	-- Remap paste for clipboard history compatibility
+	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 }
+
+return config
