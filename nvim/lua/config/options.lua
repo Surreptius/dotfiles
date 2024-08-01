e6ff5c8f9be88cc669364c5ecefcbba15f54abdd
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-08-01 17:11:15 +0200

    Established options for future tinkering

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,32 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.spell = true
-vim.opt.spelllang = { "en", "de" }
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+
+-- UI/General
+opt.number = true
+opt.relativenumber = true
+opt.ignorecase = true
+opt.cursorline = true
+opt.clipboard = "unnamedplus"
+opt.termguicolors = true
+opt.confirm = true
+opt.mouse = "a"
+opt.undofile = true
+opt.swapfile = false
+opt.conceallevel = 1
+opt.scrolloff = 12
+opt.wrap = true
+opt.linebreak = true
+opt.showtabline = 0
+opt.spelllang = { "en", "de" }
+opt.spell = false
+
+-- Set tab width
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.expandtab = true
