dadd14097af0def0561be62b4192c0e08391181d
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-08-01 17:09:22 +0200

    Removed comment on new line and spell only on md/txt files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,10 +2,33 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
+local autocmds = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- Creates a new autocommand group and removes all commands from this group
+local general = augroup("General Settings", { clear = true })
+
+autocmds("BufEnter", {
   callback = function()
-    vim.bo.textwidth = 80
-    vim.bo.formatoptions = vim.bo.formatoptions .. "t"
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
   end,
+  group = general,
+  desc = "Disable New Line Comment",
+})
+
+autocmds("BufEnter", {
+  pattern = { "*.md", "*.txt" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+  group = general,
+  desc = "Enables Spell Checking on Text Files",
+})
+
+autocmds({ "FileType" }, {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.textwidth = 80
+  end,
+  desc = "Set textwidth to 80 for markdown and text files",
 })
