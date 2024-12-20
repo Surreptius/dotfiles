3e5eea6ca07948b1ebaad53463ec7e8e0b9dcacf
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-12-20 20:55:59 +0100

    LV Update replacing cmp and telescope

--- nvim/.config/nvim/lua/plugins/core/disable.lua
+++ nvim/.config/nvim/lua/plugins/core/disable.lua
@@ -1,9 +1,8 @@
 return {
-  -- Hard Disable
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "akinsho/flash.nvim", enabled = false },
-  -- Replaced
-  { "nvim-lualine/lualine.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  -- { "hrsh7th/nvim-cmp", enabled = false },
+    -- Hard Disable
+    { "akinsho/bufferline.nvim", enabled = false },
+    { "akinsho/flash.nvim", enabled = false },
+    -- Replaced
+    -- { "nvim-lualine/lualine.nvim", enabled = false },
+    -- { "folke/noice.nvim", enabled = false },
 }
