e50eb9308cc15681ca7cf0bdb54883ba18aecc47
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-12-20 20:54:16 +0100

    Moving away from nvchad in order to have "ownership" of colorschemes

--- nvim/.config/nvim/lua/plugins/core/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/core/colorscheme.lua
@@ -1,9 +1,69 @@
-local M = {
-  "LazyVim/LazyVim",
-}
+local colorschemes = {}
 
-M.opts = {
-  colorscheme = "nvchad",
-}
+table.insert(colorschemes, {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.g.sonokai_enable_italic = true
+        vim.g.sonokai_style = "espresso"
+        vim.g.sonokai_transparent_background = 1
+    end,
+})
 
-return M
+table.insert(colorschemes, {
+    "rebelot/kanagawa.nvim",
+    opts = {
+        transparent = true,
+        colors = {
+            theme = {
+                all = { ui = { bg_gutter = "none" } },
+            },
+        },
+    },
+})
+
+table.insert(colorschemes, {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+    },
+})
+
+table.insert(colorschemes, {
+    "p00f/alabaster.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+})
+
+table.insert(colorschemes, {
+    "aktersnurra/no-clown-fiesta.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+    },
+})
+
+table.insert(colorschemes, {
+    "vague2k/vague.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+    },
+})
+
+table.insert(colorschemes, {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+    },
+})
+
+return colorschemes
