fbff01c0c621fb7916dd6306298ebac1132438d0
Paul-Ole Lang <paul-ole.lang@smail.th-koeln.de>
2024-12-20 21:32:13 +0100

    Show hidden files by default

--- nvim/.config/nvim/lua/plugins/core/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/core/neo-tree.lua
@@ -1,14 +1,17 @@
 local M = {
-  "nvim-neo-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
 }
 
 M.opts = {
-  window = {
-    position = "right",
-  },
-  filesystem = {
-    hijack_netrw_behavior = "open_current",
-  },
+    window = {
+        position = "right",
+    },
+    filesystem = {
+        hijack_netrw_behavior = "open_current",
+        filtered_items = {
+            hide_dotfiles = false,
+        },
+    },
 }
 
 return M
